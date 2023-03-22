b9b531d1f68897d39e596d14df3283049dacdbd3
rajashrikhadke <rajashrikhadke00@gmail.com>
2023-03-22 13:33:28 +0530

    Added arraylistdemo file

---
+++ project/src/project/arraylistdemo.java
@@ -0,0 +1,14 @@
+package project;
+
+import java.util.ArrayList;
+
+public class arraylistdemo {
+	public static void main(String[] args) {
+		ArrayList<Integer> list =new ArrayList<>();
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		System.out.println(list);
+	}
+
+}
