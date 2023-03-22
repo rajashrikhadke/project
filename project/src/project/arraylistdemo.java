124da0c4bb119914ca796e22875b620cd106eba6
rajashrikhadke <rajashrikhadke00@gmail.com>
2023-03-22 14:09:43 +0530

    Added a new arraylistdemo

--- project/src/project/arraylistdemo.java
+++ project/src/project/arraylistdemo.java
@@ -1,14 +1,20 @@
 package project;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class arraylistdemo {
 	public static void main(String[] args) {
-		ArrayList<Integer> list =new ArrayList<>();
-		list.add(20);
-		list.add(30);
-		list.add(40);
+		ArrayList<String> list =new ArrayList<>();
+		list.add("20");
+		list.add("30");
+		list.add("40");
+		list.add("Rajashri");
 		System.out.println(list);
+		
+		List<Integer> al = Arrays.asList(10,20,30,40);
+		System.out.println(al);
 	}
 
 }
